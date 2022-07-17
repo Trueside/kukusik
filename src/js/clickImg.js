938cd05ac7ff1d3439b0152f51e12c8cca672de2
Mikoguu <lol.nanny@mail.com>
2022-07-17 16:18:43 +0300

    js commit

--- src/js/clickImg.js
+++ src/js/clickImg.js
@@ -1,3 +1,9 @@
 function changeImg () {
-    document.getElementById('#series').setAttribute('src', 'src/img/friends.png');
+    let myImg=document.getElementById('series');
+    let imgSrc=myImg.getAttribute('src')
+    if (imgSrc === 'src/img/series.jpg') {
+        myImg.setAttribute('src', 'src/img/friends.png')
+    } else {
+        myImg.setAttribute('src', 'src/img/series.jpg') 
+    }
 }
